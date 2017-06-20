9d4f9b5828ab22c61117c979a44dd7513118f78a
Kelvely <Kelvely@inkme.cc>
2017-06-20 08:40:25 +0800

    Common commit

---
+++ src/cc/flintstone/bluej_x_templ/MainClass.java
@@ -0,0 +1,37 @@
+package cc.flintstone.bluej_x_templ;
+
+import bluej.extensions.BlueJ;
+import bluej.extensions.Extension;
+
+public class MainClass extends Extension {
+	
+	private BlueJ blueJ;
+	
+	private static final String NAME = "";
+	private static final String VERSION = "";
+
+	@Override
+	public String getName() {
+		return NAME;
+	}
+
+	@Override
+	public String getVersion() {
+		return VERSION;
+	}
+
+	@Override
+	public boolean isCompatible() {
+		return true;
+	}
+
+	@Override
+	public void startup(BlueJ bluej) {
+		this.blueJ = bluej;
+	}
+	
+	public BlueJ getBlueJ() {
+		return blueJ;
+	}
+
+}
